a98a1e8b5a116aba29e1cb3cd27c4efa434bfc08
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-16 09:26:37 -0400

    added source

---
+++ Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace socketclient
+{
+    class CLIENT_SIDE
+    {
+        static Socket client;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("CLIENT TERMINAL");
+            string host = Dns.GetHostName();
+
+            IPAddress address = Dns.GetHostEntry(host).AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(address, 1234);
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(endPoint);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace csharpsockets
+{
+    class SERVER_SIDE
+    {
+        static Socket server;
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("SERVER TERMINAL");
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string host = Dns.GetHostName();
+            int port = 1234;
+
+            IPAddress address = Dns.GetHostEntry(host).AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(address, port);
+
+            server.Bind(endPoint);
+            server.Listen(5);
+            Console.WriteLine("server connected");
+
+            while (true) {
+                Socket client = server.Accept();
+                IPEndPoint clientEndPoint = client.RemoteEndPoint as IPEndPoint;
+
+                Console.WriteLine("client connected from " + clientEndPoint.Address);
+                byte[] welcomeBytes = Encoding.ASCII.GetBytes("welcome to the server");
+
+                client.Send(welcomeBytes);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
