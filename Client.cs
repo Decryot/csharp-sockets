7e17f2e4c579e1c76176dc5583d870635dc8f00e
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-17 07:59:38 -0400

    fixed connection to server

--- Client.cs
+++ Client.cs
@@ -2,15 +2,24 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace socketclient
 {
     class CLIENT_SIDE
     {
         static Socket client;
+        static string past;
         public static void Main(string[] args)
         {
             Console.WriteLine("[CLIENT TERMINAL]");
+            Console.WriteLine(" ");
+            connectToServer();
+
+            AppDomain.CurrentDomain.ProcessExit += Exit;
+        }
+
+        static void connectToServer() {
             string host = Dns.GetHostName();
 
             IPAddress address = Dns.GetHostEntry(host).AddressList[0];
@@ -18,20 +27,34 @@ namespace socketclient
 
             client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try {
+                IPEndPoint localEndPoint;
                 client.Connect(endPoint);
-                IPEndPoint localEndPoint = client.RemoteEndPoint as IPEndPoint;
+                localEndPoint = client.RemoteEndPoint as IPEndPoint;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("successfully connected to the server");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("[IP: {0} PORT: {1}]", localEndPoint.Address, localEndPoint.Port);
+                Console.WriteLine(" ");
+
                 byte[] bytes = new byte[256];
 
                 int a = client.Receive(bytes);
-                Console.WriteLine(Encoding.UTF8.GetString(bytes));
-
-                Console.WriteLine("connected to the server");
-                Console.WriteLine("[IP: " + localEndPoint.Address + " PORT: " + localEndPoint.Port + "]");
-
-
-            }catch (Exception e) {
-                Console.WriteLine("the server does not exist");
+                if (Encoding.UTF8.GetString(bytes) != past) {
+                    Console.WriteLine("SERVER MESSAGE: '{0}'", Encoding.UTF8.GetString(bytes));
+                    past = Encoding.UTF8.GetString(bytes);
+                }
+                Console.WriteLine(" ");
             }
+            catch (Exception e) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("error found while connecting to '{0}': retrying in 5 seconds...",address);
+                Thread.Sleep(5000);
+                connectToServer();
+            }
+        }
+
+        static void Exit(object sender, EventArgs e) {
+            client.Disconnect(true);
         }
     }
 }
